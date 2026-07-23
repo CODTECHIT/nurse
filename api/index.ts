4fca72711bbcd31b6d5ef4f1d8b36fbdc11c88d6
CODTECHIT <Codtechitsolutions@gmail.com>
2026-07-23 23:13:14 +0530

    Switch to Vercel Node.js runtime to fix module compatibility

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,6 @@
 // @ts-ignore
 import server from "../dist/server/server.js";
 
-export const config = {
-  runtime: 'edge',
-};
 
 export default function(request: Request) {
   return server.fetch(request, process.env, {});
