0c0a30e36b20556bac840cb5c5eae2a2133fdd1a
CODTECHIT <Codtechitsolutions@gmail.com>
2026-07-23 23:15:42 +0530

    Fix ERR_INVALID_URL by ensuring Request URL is absolute

--- api/index.ts
+++ api/index.ts
@@ -3,5 +3,12 @@ import server from "../dist/server/server.js";
 
 
 export default function(request: Request) {
-  return server.fetch(request, process.env, {});
+  let url = request.url;
+  if (url.startsWith('/')) {
+    const host = request.headers.get('x-forwarded-host') || request.headers.get('host') || 'localhost';
+    const protocol = request.headers.get('x-forwarded-proto') || 'https';
+    url = `${protocol}://${host}${url}`;
+  }
+  const absoluteRequest = new Request(url, request);
+  return server.fetch(absoluteRequest, process.env, {});
 }
